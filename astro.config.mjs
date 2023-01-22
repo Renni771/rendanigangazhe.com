885ff42af7d7f0ebb26e0603652ce5b5bcb2592b
Rendani Gangazhe <rendanig@gmail.com>
2023-01-22 15:45:37 +0100

    feat: Add automatic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://rendanigangazhe.com/',
+  integrations: [tailwind(), sitemap()]
 });
