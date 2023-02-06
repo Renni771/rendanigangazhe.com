3e5387b544488d09535ef9174f2d3d6bf0edfc80
Rendani Gangazhe <rendanig@gmail.com>
2023-02-06 23:47:14 +0100

    chore: Use Astro `Image` component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://rendanigangazhe.com/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), image()]
 });
